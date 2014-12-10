d948fbd44ee383a1be7cd50e0c5261fa1587b292
jwright04 <jcwright04@gmail.com>
2014-12-10 11:14:30 -0800

    added jade to project seed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 /*
- * Default Gruntfile for AppGyver Steroids
+ * Default Gruntfile for Ionic Angular AppGyver Steroids
  * http://www.appgyver.com
+ * http://www.appgyver.com/steroids
+ * http://www.ionicframework.com
  *
  * Licensed under the MIT license.
  */
@@ -9,8 +11,38 @@
 
 module.exports = function(grunt) {
 
+  //load tasks
   grunt.loadNpmTasks("grunt-steroids");
+  grunt.loadNpmTasks("grunt-contrib-jade");
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask("default", ["steroids-make", "steroids-compile-sass"]);
+  // jade configuration
+  // must include grunt watch in seperate terminal window
+  //TO DO: Modify so that only one terminal is needed to watch for jade 
+  grunt.initConfig({
+    jade: {
+      html: {
+        files: [{
+          expand: true,
+          cwd: 'www/templates/jade',
+          src: ['**/*.jade'],
+          dest: 'www/templates',
+          ext: '.html'
+        }],
+        options:{
+          pretty: true
+        }
+      }
+    },
 
+    watch:{
+      files: ['**/*.jade'],
+      tasks: ['jade']
+    }
+  });
+
+  grunt.registerTask("default", [
+    "steroids-make", 
+    "steroids-compile-sass"
+    ]);
 };
--- www/js/controllers/petIndex.js
+++ www/js/controllers/petIndex.js
@@ -6,10 +6,3 @@ angular.module('starter.controllers', [])
   // "Pets" is a service returning mock data (services.js)
   $scope.pets = PetService.all();
 })
-
-
-// A simple controller that shows a tapped item's data to
-.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pet = PetService.get($stateParams.petId);
-});
